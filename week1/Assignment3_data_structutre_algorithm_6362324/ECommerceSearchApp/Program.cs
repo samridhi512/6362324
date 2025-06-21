aa9285da0094f792d88ec74ac324b3efac87cf96
samridhi512 <146466483+samridhi512@users.noreply.github.com>
2025-06-21 23:58:54 +0530

    Add files via upload

---
+++ week1/Assignment3_data_structutre_algorithm_6362324/ECommerceSearchApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Product[] products = new Product[]
+        {
+            new Product(103, "Keyboard", "Electronics"),
+            new Product(101, "Shoes", "Footwear"),
+            new Product(105, "Mouse", "Electronics"),
+            new Product(102, "T-shirt", "Apparel"),
+            new Product(104, "Bag", "Accessories"),
+        };
+
+        Console.WriteLine("=== Linear Search ===");
+        Console.Write("Enter product name to search: ");
+        string name = Console.ReadLine();
+        Product result1 = SearchFunctions.LinearSearch(products, name);
+        Console.WriteLine(result1 != null ? result1.ToString() : "Product not found");
+
+        
+        Array.Sort(products, (p1, p2) => p1.ProductId.CompareTo(p2.ProductId));
+
+        Console.WriteLine("\n=== Binary Search ===");
+        Console.Write("Enter product ID to search: ");
+        int id = int.Parse(Console.ReadLine());
+        Product result2 = SearchFunctions.BinarySearch(products, id);
+        Console.WriteLine(result2 != null ? result2.ToString() : "Product not found");
+
+        Console.ReadLine();
+    }
+}
---
+++ week1/Assignment3_data_structutre_algorithm_6362324/ECommerceSearchApp/SearchFunctions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SearchFunctions
+{
+    // Linear Search by Product Name
+    public static Product LinearSearch(Product[] products, string name)
+    {
+        foreach (Product p in products)
+        {
+            if (p.ProductName.Equals(name, StringComparison.OrdinalIgnoreCase))
+                return p;
+        }
+        return null;
+    }
+
+    // Binary Search by Product ID
+    public static Product BinarySearch(Product[] products, int productId)
+    {
+        int low = 0;
+        int high = products.Length - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if (products[mid].ProductId == productId)
+                return products[mid];
+            else if (products[mid].ProductId < productId)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return null;
+    }
+}
