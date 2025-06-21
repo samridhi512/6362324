146df329359782b18a886ed934e6fd144f81f4be
samridhi512 <146466483+samridhi512@users.noreply.github.com>
2025-06-21 23:14:38 +0530

    Add files via upload

---
+++ week1/Assignment1_design_pattern_6362324/SingletonPattern/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SingletonPatternExample
+{
+    public class Logger
+    {
+        
+        private static Logger instance = null;
+
+        private static readonly object lockObj = new object();
+
+        
+        private Logger()
+        {
+            Console.WriteLine("Logger initialized.");
+        }
+
+        
+        public static Logger GetInstance()
+        {
+            // Double-check locking for thread safety
+            if (instance == null)
+            {
+                lock (lockObj)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        
+        public void Log(string message)
+        {
+            Console.WriteLine($"[LOG]: {message}");
+        }
+    }
+}
+
---
+++ week1/Assignment1_design_pattern_6362324/SingletonPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Singleton Logger...\n");
+
+            Logger logger1 = Logger.GetInstance();
+            logger1.Log("This is the first log message.");
+
+            Logger logger2 = Logger.GetInstance();
+            logger2.Log("This is the second log message.");
+
+            
+            if (object.ReferenceEquals(logger1, logger2))
+            {
+                Console.WriteLine("\nBoth logger1 and logger2 reference the SAME instance. Singleton works!");
+            }
+            else
+            {
+                Console.WriteLine("\nDifferent instances were created. Singleton FAILED.");
+            }
+        }
+    }
+}
