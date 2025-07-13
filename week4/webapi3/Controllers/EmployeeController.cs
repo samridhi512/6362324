1e2546b9595eacb79cf7c6ec34433b59f5106c2b
Samridhi <shreesamridhi503@gmail.com>
2025-07-14 00:50:49 +0530

    Added WebAPI3 assignment files in Week4

---
+++ week4/webapi3/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi3.Filters;
+using webapi3.Models;
+
+namespace webapi3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [ServiceFilter(typeof(CustomAuthFilter))]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> _employees = new List<Employee>();
+
+        public EmployeeController()
+        {
+            if (_employees.Count == 0)
+            {
+                _employees = GetStandardEmployeeList();
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public ActionResult<List<Employee>> Get()
+        {
+            return Ok(_employees);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Employee emp)
+        {
+            _employees.Add(emp);
+            return Ok($"Added: {emp.Name}");
+        }
+
+        private List<Employee> GetStandardEmployeeList()
+        {
+            return new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Samridhi",
+                    Salary = 50000,
+                    Permanent = true,
+                    Department = new Department { Id = 101, Name = "HR" },
+                    Skills = new List<Skill> { new Skill { Id = 1, Name = "Java" } },
+                    DateOfBirth = new DateTime(2000, 1, 1)
+                }
+            };
+        }
+    }
+}
