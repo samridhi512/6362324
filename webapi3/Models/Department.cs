587d16f4b062e12f7bfd866314893aa3843aa147
samridhi512 <146466483+samridhi512@users.noreply.github.com>
2025-07-14 00:43:26 +0530

    Delete webapi3 directory

--- webapi3/Models/Department.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webapi3.Models
-{
-    public class Department
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- webapi3/Models/Skill.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webapi3.Models
-{
-    public class Skill
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
